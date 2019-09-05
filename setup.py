3b61074a07fc8cbd09c307afbcd16da58d38bf62
Unknown <p.thorn.ru@gmail.com>
2019-09-05 08:40:10 +0300

    Release v2.1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 
 setup(
     name='eor-filestore',
-    version='2.0.0',
+    version='2.1.0',
     description='A file upload and storage backend for the Pyramid framework',
     long_description='',
     classifiers=[
